539dbf5f873edefe52b3d48683f231e1b8db9daa
juri turiano <juricuber@gmail.com>
2025-04-22 10:34:13 +0800

    Add: Leaderboard page HomeScreen page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
-import Ranks from "./pages/HomeScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar, TouchableOpacity } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Leaderboard from "./pages/Leaderboard";
+import HomeScreen from "./pages/Homescreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        headerStyle: { height: 50 },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Leaderboard" component={Leaderboard} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar translucent={true} barStyle="light-content" />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Leaderboards" component={Ranks} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="BottomTabs"
+          component={TabNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Leaderboard" component={Leaderboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- pages/HomeScreen.tsx
+++ pages/HomeScreen.tsx
@@ -1,30 +1,14 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image, Pressable } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function Ranks() {
+export default function HomeScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            ducimus impedit voluptatem doloribus nihil dicta, unde fugit nostrum
-            porro temporibus totam tempora quas consectetur, aperiam repellendus
-            amet laudantium beatae officiis.
-          </Text>
+      <SafeAreaView>
+        <View>
+          <Text>Hello!</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: "black",
-  },
-
-  container: {
-    padding: 10,
-  },
-});
